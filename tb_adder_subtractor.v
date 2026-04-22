3b1937dcc50bb2a5c667fb7b93298f5c0d9067b3
LoopyNoOne <157922522+LoopyNoOne@users.noreply.github.com>
2026-04-22 13:28:43 +0100

    Update tb_adder_subtractor.v
    Added divison test and fixed some bugs with setup

--- tb_adder_subtractor.v
+++ tb_adder_subtractor.v
@@ -3,19 +3,21 @@
 module tb_adder_subtractor;
 
     // Declare inputs as reg, outputs as wire
-    reg  signed [7:0] A;
-    reg  signed [7:0] B;
-    reg               mode;
-    wire signed [7:0] result;
+    reg  signed [9:0] A;
+    reg  signed [9:0] B;
+    reg         [1:0] mode;
+    wire signed [9:0] result;
     wire              overflow;
+    wire              div_by_zero;
 
     // Instantiate DUT
-    adder_subtractor uut (
+    adder_subtractor_divider uut (
         .A        (A),
         .B        (B),
         .mode     (mode),
         .result   (result),
-        .overflow (overflow)
+        .overflow (overflow),
+        .div_by_zero(div_by_zero)
     );
 
     initial begin
@@ -24,27 +26,35 @@ module tb_adder_subtractor;
 
         //  ADDITION 
         // TC1: 15 + 10 = 25
-        #10; A = 8'd15;   B = 8'd10;   mode = 0;
+        #10; A = 10'd15;   B = 10'd10;   mode = 0;
         // TC2: 0 + 42 = 42
-        #10; A = 8'd0;    B = 8'd42;   mode = 0;
+        #10; A = 10'd0;    B = 10'd42;   mode = 0;
         // TC3: -20 + 50 = 30
-        #10; A = -8'd20;  B = 8'd50;   mode = 0;
+        #10; A = -10'd20;  B = 10'd50;   mode = 0;
         // TC4: -10 + -15 = -25
-        #10; A = -8'd10;  B = -8'd15;  mode = 0;
+        #10; A = -10'd10;  B = -10'd15;  mode = 0;
         // TC5: 127 + 1 = overflow
-        #10; A = 8'sd127; B = 8'sd1;   mode = 0; // sd = signed decimal
+        #10; A = 10'sd127; B = 10'sd1;   mode = 0; // sd = signed decimal
 
         //  SUBTRACTION 
         // TC6: 30 - 10 = 20
-        #10; A = 8'd30;   B = 8'd10;   mode = 1;
+        #10; A = 10'd30;   B = 10'd10;   mode = 1;
         // TC7: 55 - 55 = 0
-        #10; A = 8'd55;   B = 8'd55;   mode = 1;
+        #10; A = 10'd55;   B = 10'd55;   mode = 1;
         // TC8: 5 - 20 = -15
-        #10; A = 8'd5;    B = 8'd20;   mode = 1;
+        #10; A = 10'd5;    B = 10'd20;   mode = 1;
         // TC9: (-10) - (-30) = 20
-        #10; A = -8'd10;  B = -8'd30;  mode = 1;
+        #10; A = -10'd10;  B = -10'd30;  mode = 1;
         // TC10: 127 - -1 = overflow
-        #10; A = 8'sd127; B = -8'sd1;  mode = 1; // sd = signed decimal
+        #10; A = 10'sd127; B = -10'sd1;  mode = 1; // sd = signed decimal
+        
+        
+        // DIVISION 
+        #10; A = 10'sd40;  B = 10'sd5;   mode = 2'b10; // 40 / 5 = 8
+        #10; A = -10'sd30; B = 10'sd3;   mode = 2'b10; // -10
+        #10; A = 10'sd25;  B = -10'sd5;  mode = 2'b10; // -5
+        #10; A = 10'sd10;  B = 10'sd0;   mode = 2'b10; // div by zero
+
 
         #10; $finish;
     end
